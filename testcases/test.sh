275b46aef261ac65808d2c1ea0033536bda06b22
Alexander F Rødseth <rodseth@gmail.com>
2015-03-03 16:15:42 +0100

    Added testcase that fails

--- testcases/test.sh
+++ testcases/test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# TODO: Create one function for preparing and one for diffing+removing
+#       temporary test files.
+
 echo -n 'Testing PKGBUILD...'
 rm -f PKGBUILD
 cp PKGBUILD.orig PKGBUILD
 ../setconf.py PKGBUILD pkgname=asdfasdf
 ../setconf.py PKGBUILD pkgrel+=1
-diff PKGBUILD PKGBUILD.correct && echo 'ok' || echo 'FAIL'
-rm -f PKGBUILD
+diff PKGBUILD PKGBUILD.correct && (echo 'ok'; rm -f PKGBUILD) || echo 'FAIL'
 
 echo -n 'Testing testcase2...'
 rm -f testcase2
@@ -14,5 +16,11 @@ cp testcase2.orig testcase2
 ../setconf.py testcase2 x+=2
 ../setconf.py testcase2 x-=3
 ../setconf.py testcase2 z+=1000
-diff testcase2 testcase2.correct && echo 'ok' || echo 'FAIL'
-rm -f testcase2
+diff testcase2 testcase2.correct && (echo 'ok'; rm -f testcase2) || echo 'FAIL'
+
+echo -n 'Testing nonewline...'
+rm -f nonewline
+cp nonewline.orig nonewline
+../setconf.py nonewline x=3
+diff nonewline nonewline.correct && (echo 'ok'; rm -f nonewline) || echo 'FAIL'
+
